d5c941fae762f3e6f2a15d628c6a3434dc13c047
Alison Kline <alsnkline@gmail.com>
2023-10-31 17:39:56 -0700

    correcting file extension

---
+++ second_python/main.py
@@ -0,0 +1,32 @@
+from faker import Faker
+
+def main(request):
+    (insertTransactions, deleteTransactions, newTransactionCursor) = generateProfile()
+    insert = {}
+    insert['transactions'] = insertTransactions
+    delete = {}
+    delete['transactions'] = deleteTransactions
+    state = {}
+    state['transactionsCursor'] = newTransactionCursor
+    transactionsSchema = {}
+    transactionsSchema['primary_key'] = ['username', 'sex']
+    schema = {}
+    schema['transactions'] = transactionsSchema
+    response = {}
+    
+    # Add updated state to response
+    response['state'] =  state
+    response['insert'] = insert
+    response['delete'] = delete
+    response['schema'] = schema
+    response['hasMore'] = False
+    print("Returning the response...")
+    return response
+
+def generateProfile():
+    fake = Faker();
+    profile = fake.simple_profile();
+    insertTransactions = []
+    insertTransactions.append(profile);
+    newTransactionCursor=1
+    return (insertTransactions, [], newTransactionCursor)
